e50d4d29c0fa23406e050edd1d15e915bfdd1cf0
Wusssc <0962054442cindy@gmail.com>
2024-05-20 15:54:43 +0800

    first commit

---
+++ working_hours/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace working_hours
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvAssignments = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbEmployees = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbItems = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbUnits = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMachines = new System.Windows.Forms.ComboBox();
+            this.btnAssign = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignments)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label5.Location = new System.Drawing.Point(478, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "狀態";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "正在進行",
+            "暫停",
+            "已完成"});
+            this.comboBox1.Location = new System.Drawing.Point(465, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(69, 20);
+            this.comboBox1.TabIndex = 44;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button1.Location = new System.Drawing.Point(768, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "更改狀態";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvAssignments);
+            this.panel1.Location = new System.Drawing.Point(18, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(914, 408);
+            this.panel1.TabIndex = 42;
+            // 
+            // dgvAssignments
+            // 
+            this.dgvAssignments.AllowUserToDeleteRows = false;
+            this.dgvAssignments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAssignments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAssignments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAssignments.Location = new System.Drawing.Point(0, 0);
+            this.dgvAssignments.Name = "dgvAssignments";
+            this.dgvAssignments.RowTemplate.Height = 24;
+            this.dgvAssignments.Size = new System.Drawing.Size(914, 408);
+            this.dgvAssignments.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label4.Location = new System.Drawing.Point(306, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "名字";
+            // 
+            // cmbEmployees
+            // 
+            this.cmbEmployees.FormattingEnabled = true;
+            this.cmbEmployees.Location = new System.Drawing.Point(293, 41);
+            this.cmbEmployees.Name = "cmbEmployees";
+            this.cmbEmployees.Size = new System.Drawing.Size(69, 20);
+            this.cmbEmployees.TabIndex = 40;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(197, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "操作項目";
+            // 
+            // cmbItems
+            // 
+            this.cmbItems.FormattingEnabled = true;
+            this.cmbItems.Location = new System.Drawing.Point(200, 41);
+            this.cmbItems.Name = "cmbItems";
+            this.cmbItems.Size = new System.Drawing.Size(69, 20);
+            this.cmbItems.TabIndex = 38;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(106, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "機台單元";
+            // 
+            // cmbUnits
+            // 
+            this.cmbUnits.FormattingEnabled = true;
+            this.cmbUnits.Location = new System.Drawing.Point(109, 41);
+            this.cmbUnits.Name = "cmbUnits";
+            this.cmbUnits.Size = new System.Drawing.Size(69, 20);
+            this.cmbUnits.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "機台名稱";
+            // 
+            // cmbMachines
+            // 
+            this.cmbMachines.FormattingEnabled = true;
+            this.cmbMachines.Items.AddRange(new object[] {
+            ""});
+            this.cmbMachines.Location = new System.Drawing.Point(19, 41);
+            this.cmbMachines.Name = "cmbMachines";
+            this.cmbMachines.Size = new System.Drawing.Size(69, 20);
+            this.cmbMachines.TabIndex = 34;
+            // 
+            // btnAssign
+            // 
+            this.btnAssign.Font = new System.Drawing.Font("新細明體", 14F);
+            this.btnAssign.Location = new System.Drawing.Point(657, 32);
+            this.btnAssign.Name = "btnAssign";
+            this.btnAssign.Size = new System.Drawing.Size(82, 39);
+            this.btnAssign.TabIndex = 33;
+            this.btnAssign.Text = "派工";
+            this.btnAssign.UseVisualStyleBackColor = true;
+            this.btnAssign.Click += new System.EventHandler(this.btnAssign_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbEmployees);
+            this.groupBox1.Controls.Add(this.cmbMachines);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbUnits);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbItems);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(43, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 79);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 518);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAssign);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignments)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvAssignments;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbEmployees;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbItems;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbUnits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbMachines;
+        private System.Windows.Forms.Button btnAssign;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ working_hours/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace working_hours
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        private Dictionary<int, TimeSpan> elapsedTimes = new Dictionary<int, TimeSpan>(); // 儲存每個行的已過時間
+        private Dictionary<int, DateTime> startTimes = new Dictionary<int, DateTime>(); // 儲存每個行的開始時間
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 初始化 Timer
+            timer = new Timer();
+            timer.Interval = 1000; // 1 秒
+            timer.Tick += timer1_Tick;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // 連線到 SQLite 資料庫
+            string connectionString = "Data Source=C:\\Users\\User\\source\\repos\\working_hours\\mydatabase.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // 填入機台名稱選單
+                string query = "SELECT MachineID, MachineName FROM Machines";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    cmbMachines.ValueMember = "MachineID";
+                    cmbMachines.DisplayMember = "MachineName";
+                    cmbMachines.DataSource = table;
+                }
+
+                // 填入機台單元選單
+                query = "SELECT UnitID, UnitName FROM MachineUnits";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    cmbUnits.ValueMember = "UnitID";
+                    cmbUnits.DisplayMember = "UnitName";
+                    cmbUnits.DataSource = table;
+                }
+
+                // 填入操作項目選單
+                query = "SELECT ItemID, ItemName FROM OperationItems";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    cmbItems.ValueMember = "ItemID";
+                    cmbItems.DisplayMember = "ItemName";
+                    cmbItems.DataSource = table;
+                }
+
+                // 填入員工名稱選單
+                query = "SELECT EmployeeID, EmployeeName FROM Employees";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    cmbEmployees.ValueMember = "EmployeeID";
+                    cmbEmployees.DisplayMember = "EmployeeName";
+                    cmbEmployees.DataSource = table;
+                }
+
+                //開啟派工資料表
+                query = "SELECT * FROM WorkAssignments";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvAssignments.DataSource = table;
+
+                    dgvAssignments.Columns.Remove("AssignmentID");
+
+
+                    // 初始化已過時間及開始時間
+                    foreach (DataGridViewRow row in dgvAssignments.Rows)
+                    {
+                        int rowIndex = row.Index + 1;
+                        elapsedTimes.Add(rowIndex, TimeSpan.Zero);
+
+                        // 檢查 "StartTime" 欄位是否為 null
+                        if (row.Cells["StartTime"].Value != null)
+                        {
+                            startTimes.Add(rowIndex, DateTime.Parse(row.Cells["StartTime"].Value.ToString()));
+
+                            // 如果狀態為"正在進行中",則啟動該行的計時
+                            if (row.Cells["State"].Value != null && row.Cells["State"].Value.ToString() == "正在進行中")
+                            {
+                                startTimes[rowIndex] = DateTime.Parse(row.Cells["StartTime"].Value.ToString());
+                            }
+                        }
+                        else
+                        {
+                            // 如果為 null,則使用當前時間作為開始時間
+                            startTimes.Add(rowIndex, DateTime.Now);
+                        }
+                    }
+                }
+            }
+            // 啟動計時器
+            timer.Start();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgvAssignments.Rows)
+            {
+                int rowIndex = row.Index + 1;
+                if (row.Cells["State"].Value != null && row.Cells["State"].Value.ToString() == "正在進行中")
+                {
+                    elapsedTimes[rowIndex] = DateTime.Now - startTimes[rowIndex];
+                    row.Cells["TotalTime"].Value = elapsedTimes[rowIndex].ToString(@"hh\:mm\:ss");
+                }
+            }
+        }
+
+        private void btnAssign_Click(object sender, EventArgs e)
+        {
+            // 獲取選擇的值
+            string machineName = cmbMachines.Text;
+            string unitName = cmbUnits.Text;
+            string itemName = cmbItems.Text;
+            string employeeName = cmbEmployees.Text;
+            DateTime startTime = DateTime.Now;
+
+            // 連線到 SQLite 資料庫
+            string connectionString = "Data Source=C:\\Users\\User\\source\\repos\\working_hours\\mydatabase.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // 插入派工記錄
+                string query = "INSERT INTO WorkAssignments (MachineName, UnitName, ItemName, EmployeeName, State, TotalTime, StartTime)" +
+                    " VALUES (@MachineName, @UnitName, @ItemName, @EmployeeName, @State, @TotalTime, @StartTime)";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@MachineName", machineName);
+                    command.Parameters.AddWithValue("@UnitName", unitName);
+                    command.Parameters.AddWithValue("@ItemName", itemName);
+                    command.Parameters.AddWithValue("@EmployeeName", employeeName);
+                    command.Parameters.AddWithValue("@State", "正在進行中");
+                    command.Parameters.AddWithValue("@StartTime", startTime.ToString("yyyy-MM-dd HH:mm:ss")); // 加入開始時間
+                    command.Parameters.AddWithValue("@TotalTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                    command.ExecuteNonQuery();
+                }
+
+                // 更新 DataGridView 中的內容
+                query = "SELECT * FROM WorkAssignments";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvAssignments.DataSource = null;
+                    dgvAssignments.Rows.Clear();
+                    dgvAssignments.DataSource = table;
+                    dgvAssignments.Columns.Remove("AssignmentID");
+
+                    // 初始化新行的已過時間
+                    int lastIndex = dgvAssignments.Rows.Count - 1;
+                    elapsedTimes.Add(lastIndex + 1, TimeSpan.Zero);
+                }
+            }
+
+            // 如果尚未啟動計時器,則啟動
+            if (!timer.Enabled)
+            {
+                timer.Start();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dgvAssignments.SelectedRows.Count > 0) // 確保至少有一行被選中
+            {
+                DataGridViewRow selectedRow = dgvAssignments.SelectedRows[0];
+                int rowIndex = selectedRow.Index + 1;
+
+                if (comboBox1.Text == "暫停" || comboBox1.Text == "已完成")
+                {
+                    // 停止選中行的計時
+                    elapsedTimes[rowIndex] = DateTime.Now - startTimes[rowIndex];
+                    selectedRow.Cells["TotalTime"].Value = elapsedTimes[rowIndex].ToString(@"hh\:mm\:ss");
+                    selectedRow.Cells["State"].Value = comboBox1.Text;
+                }
+                else
+                {
+                    // 啟動選中行的計時
+                    startTimes[rowIndex] = DateTime.Now;
+                    selectedRow.Cells["State"].Value = "正在進行中";
+                }
+            }
+        }
+    }
+}
